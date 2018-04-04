15c56bbe0f5c5c49a91d17ee49083a533f4d51a8
Dylan Katz <muddmaker.97@gmail.com>
2018-04-03 18:31:00 -0700

    make output useful

--- tcp_server.py
+++ tcp_server.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2018 The GrayHats Cybersecurity Club
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
 from socket import getaddrinfo, socket, AF_UNSPEC, SOCK_STREAM, AI_PASSIVE
 from sys import argv, exit
 
-if len(argv) < 2:
-    print('err: too few args')
+if len(argv) != 2:
+    print('err: needs one argument')
     print('usage: {} port'.format(argv[0]))
     exit(1)
 
@@ -29,16 +44,27 @@ for r in getaddrinfo(None, int(argv[1]), AF_UNSPEC, SOCK_STREAM, 0, AI_PASSIVE):
     break
 
 if s is None:
-    print('Could Not Open Socket')
+    print('err: failed to listen on port {}'.format(argv[1]))
     exit(1)
 
 while True:
     conn, addr = s.accept()
-    data = conn.recv(1024)
 
-    if not data:
-        break
+    try:
+        ip, port = addr
+    except ValueError:
+        ip, port, flow, scope = addr
 
-    print('Recieved {} from {}'.format(repr(data), addr))
-    conn.send(data)
+    print('new connection from {} on port {}'.format(ip, port))
+
+    while True:
+        data = conn.recv(1024)
+
+        if not data:
+            break
+
+        print('recieved "{}" from client'.format(data))
+        conn.send(data)
+
+    print('connection from {} has been closed'.format(ip))
     conn.close()
--- udp_client.py
+++ udp_client.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2018 The GrayHats Cybersecurity Club
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
 from socket import getaddrinfo, socket, AF_UNSPEC, SOCK_DGRAM
 from sys import argv, exit
 
-if len(argv) < 3:
-    print('err: too few args')
+if len(argv) != 3:
+    print('err: needs two arguments')
     print('usage: {} host port'.format(argv[0]))
     exit(1)
 
@@ -28,10 +43,10 @@ for r in getaddrinfo(argv[1], int(argv[2]), AF_UNSPEC, SOCK_DGRAM):
     break
 
 if s is None:
-    print('could not open socket')
+    print('err: failed to connect to {} on port {}'.format(argv[1], argv[2]))
     exit(1)
 
 s.sendall(b'Hello, world')
 data = s.recv(1024)
 s.close()
-print('Received {}'.format(repr(data)))
+print('received "{}" back from server'.format(data))
